dbdbd95ddbdd3c219a1bf71c26c642e576aac0dd
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-17 20:41:57 +0000

    Mostly done placeholder text

--- scripts/js/universal.js
+++
@@ -1,5 +0,0 @@
-const typed = new Typed('#typed', {
-  stringsElement: '.typed-strings',
-  typeSpeed: 70,
-  loop: false,
-});
--- scripts/minified/universal-min.js
+++ scripts/minified/universal-min.js
@@ -1,2 +1,2 @@
-const typed=new Typed("#typed",{stringsElement:".typed-strings",typeSpeed:70,loop:!1});
+
 //# sourceMappingURL=universal-min.js.map
