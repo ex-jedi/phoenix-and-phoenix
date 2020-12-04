a8de5ce30c3fb316000e117a49fb51ff41eb9532
Mark <mark@pixelsmiths.co.uk>
2020-12-04 18:06:39 +0000

    Bio region and markup

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('homepage-header'); ?>
 <main class="main-content homepage-main-content" id="main-content">
-<h1>Ahoy</h1>
+<?php perch_content("Section One"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('main-footer-end-js'); ?>
