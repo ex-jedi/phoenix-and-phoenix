61c78aad28fb52d8e9671658c59ddfe5eea05352
Mark <mark@pixelsmiths.co.uk>
2020-12-04 15:17:07 +0000

    Main logo sizing

--- perch/templates/layouts/main-header.php
+++ perch/templates/layouts/main-header.php
@@ -6,6 +6,8 @@
 	<link rel="stylesheet" href="/css/main.css?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/css/main.css');?>">
 	<?php $page_css_file	=  perch_page_attribute('css', array('template' => 'bits.html'), true); ?>
 	<link rel="stylesheet" href="<?php echo $page_css_file ?>?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].$page_css_file);?>">
+	<!-- Typekit  -->
+	<link rel="stylesheet" href="https://use.typekit.net/xvs0zel.css">
 	<!-- Perch Meta -->
 	<?php
 	$domain        = 'https://'.$_SERVER["HTTP_HOST"];
