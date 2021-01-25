8d99e53d82a107ff309e93c719d31b6bcfe07fc9
Mark <mark@pixelsmiths.co.uk>
2021-01-25 16:17:24 +0000

    Fixing Perch template

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <main class="main-content homepage-main-content" id="main-content">
 <?php perch_content("Section One"); ?>
 <?php perch_content("Section Two"); ?>
+<?php perch_content("Testimonials Section"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('homepage-footer-end'); ?>
