3d9c48c444ac4a995de0f7cd0340ccec47147cd7
Mark <mark@pixelsmiths.co.uk>
2021-02-08 10:23:16 +0000

    Redactor font color plugin

--- perch/addons/plugins/editors/redactor-plugins/alignment.js
+++ perch/addons/plugins/editors/redactor-plugins/alignment.js
@@ -1,54 +1,48 @@
-(function($R)
-{
-    $R.add('plugin', 'alignment', {
-        translations: {
-    		en: {
-    			"align": "Align",
-    			"align-left": "Align Left",
-    			"align-center": "Align Center",
-    			"align-right": "Align Right",
-    			"align-justify": "Align Justify"
-    		}
-        },
-        init: function(app)
-        {
-            this.app = app;
-            this.lang = app.lang;
-            this.block = app.block;
-            this.toolbar = app.toolbar;
-        },
-        // public
-        start: function()
-        {
-            var dropdown = {};
+(function ($R) {
+  $R.add('plugin', 'alignment', {
+    translations: {
+      en: {
+        align: 'Align',
+        'align-left': 'Align Left',
+        'align-center': 'Align Center',
+        'align-right': 'Align Right',
+        'align-justify': 'Align Justify',
+      },
+    },
+    init(app) {
+      this.app = app;
+      this.lang = app.lang;
+      this.block = app.block;
+      this.toolbar = app.toolbar;
+    },
+    // public
+    start() {
+      const dropdown = {};
 
-    		dropdown.left = { title: this.lang.get('align-left'), api: 'plugin.alignment.set', args: 'left' };
-    		dropdown.center = { title: this.lang.get('align-center'), api: 'plugin.alignment.set', args: 'center' };
-    		dropdown.right = { title: this.lang.get('align-right'), api: 'plugin.alignment.set', args: 'right' };
-    		dropdown.justify = { title: this.lang.get('align-justify'), api: 'plugin.alignment.set', args: 'justify' };
+      dropdown.left = { title: this.lang.get('align-left'), api: 'plugin.alignment.set', args: 'left' };
+      dropdown.center = { title: this.lang.get('align-center'), api: 'plugin.alignment.set', args: 'center' };
+      dropdown.right = { title: this.lang.get('align-right'), api: 'plugin.alignment.set', args: 'right' };
+      dropdown.justify = { title: this.lang.get('align-justify'), api: 'plugin.alignment.set', args: 'justify' };
 
-            var $button = this.toolbar.addButton('alignment', { title: this.lang.get('align') });
-            $button.setIcon('<i class="re-icon-alignment"></i>');
-			$button.setDropdown(dropdown);
-        },
-        set: function(type)
-		{
-    		if (type === 'left')
-    		{
-        		return this._remove();
-    		}
+      const $button = this.toolbar.addButton('alignment', { title: this.lang.get('align') });
+      $button.setIcon('<i class="re-icon-alignment"></i>');
+      $button.setDropdown(dropdown);
+    },
+    set(type) {
+      if (type === 'left') {
+        return this._remove();
+      }
 
-    		var args = {
-        	    style: { 'text-align': type }
-    		};
+      const args = {
+        style: { 'text-align': type },
+      };
 
-			this.block.toggle(args);
-		},
+      this.block.toggle(args);
+    },
 
-		// private
-		_remove: function()
-		{
-		    this.block.remove({ style: 'text-align' });
-		}
-    });
+    // private
+    _remove() {
+      this.block.remove({ style: 'text-align' });
+    },
+  });
 })(Redactor);
