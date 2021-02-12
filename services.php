e4731a615e38f86b4f1b9ae14b66a9615f44a5e8
Mark <mark@pixelsmiths.co.uk>
2021-02-12 12:58:47 +0000

    Websites tab mostly finished

--- services.php
+++ services.php
@@ -2,7 +2,6 @@
 <?php perch_layout('main-header'); ?>
 <main class="main-content about-us-main-content" id="main-content">
 <?php perch_content("Services Content"); ?>
-<?php perch_content("Contact Section"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('services-footer-end'); ?>
