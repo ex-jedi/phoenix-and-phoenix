2cb23205cf546e0ec0a261801f698b059727f506
Mark <mark@pixelsmiths.co.uk>
2021-02-18 16:02:26 +0000

    .htaccess test

--- portfolio.php
+++ portfolio.php
@@ -1,10 +1,21 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content about-us-main-content" id="main-content">
-<h1>Ahoy</h1>
+
+    <?php
+    perch_content_create('Products', array(
+         'template'   => 'product_detail.html',
+         'multiple'    => true,
+         'edit-mode' => 'listdetail',
+    ));
+
+        perch_content_custom('Products', array(
+         'template' => 'product_listing.html',
+    ));
+    ?>
+
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('main-footer-end'); ?>
 <!-- TODO: Remove debug -->
 <!-- <?php PerchUtil::output_debug(); ?> -->
-
