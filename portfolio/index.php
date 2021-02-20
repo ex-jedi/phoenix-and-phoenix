e50a496a43476deffd718f92ada0a19d2e93e834
Mark <mark@pixelsmiths.co.uk>
2021-02-20 16:07:14 +0000

    Portfolio dual image wrapper

--- portfolio/index.php
+++ portfolio/index.php
@@ -2,11 +2,10 @@
 <?php perch_layout('main-header'); ?>
 <main class="main-content about-us-main-content" id="main-content">
 <section class="portfolio-section portfolio-index-section">
-
     <?php perch_content("Portfolio Index Subheading"); ?>
-
     <?php perch_pages_navigation(array(
         'from-path' => '/portfolio',
+        'hide-extensions' => true,
         'template' => 'portfolio_menu.html',
         'levels'    => 1,
         'siblings' => false));
