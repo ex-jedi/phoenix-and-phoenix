c42654f6cf7aa3305087ed7a1de22e983afece40
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-03-04 13:34:46 +0000

    Illustration pahge setup

---
+++ illustration.php
@@ -0,0 +1,9 @@
+<?php include('perch/runtime.php'); ?>
+<?php perch_layout('main-header'); ?>
+<main class="main-content illustration-main-content" id="main-content">
+<?php perch_content("Illustration Content"); ?>
+</main>
+<?php perch_layout('main-footer'); ?>
+<?php perch_layout('main-footer-end'); ?>
+<!-- TODO: Remove debug -->
+<!-- <?php PerchUtil::output_debug(); ?> -->
