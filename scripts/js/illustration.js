4de8159d0dd00c0c8c0bbf48e67a7d2c656a64b5
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-03-04 17:19:17 +0000

    Working menu

--- scripts/js/illustration.js
+++ scripts/js/illustration.js
@@ -4,9 +4,9 @@ import 'flickity-imagesloaded';
 const carousel = document.querySelector('.illustration-portfolio-section');
 const flkty = new Flickity(carousel, {
   imagesLoaded: true,
-  autoPlay: 1000,
-  // prevNextButtons: false,
+  autoPlay: 2000,
+  prevNextButtons: false,
   wrapAround: true,
-  // selectedAttraction: 0.006,
-  // friction: 0.15,
+  selectedAttraction: 0.006,
+  friction: 0.15,
 });
