76b38a14022b289c15ea686d0e4acb013a1d5dcb
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-03-05 11:24:31 +0000

    Smaller logo

--- illustration.php
+++ illustration.php
@@ -1,7 +1,7 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('small-header'); ?>
 <main class="main-content illustration-main-content" id="main-content">
-<?php perch_content("Illustration Content"); ?>
+<?php perch_content("Illustration Images"); ?>
 </main>
 <?php perch_layout('invisible-footer'); ?>
 <?php perch_layout('illustration-footer-end'); ?>
