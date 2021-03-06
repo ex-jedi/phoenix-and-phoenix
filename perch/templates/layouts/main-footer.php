7da155dce5a10a4f88047bb148b3ffa56dad17f8
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-03-06 15:28:56 +0000

    Legals pages

--- perch/templates/layouts/main-footer.php
+++ perch/templates/layouts/main-footer.php
@@ -1,10 +1,10 @@
 <footer class="main-footer">
 		<?php perch_content("Footer Social Media"); ?>
 		<p class="footer-terms">
-			<a class="footer-link" href="/terms.php">Terms of use</a>
+			<a class="footer-link" href="/terms">Terms of use</a>
 		</p>
 		<p class="footer-privacy">
-			<a class="footer-link" href="/privacy-and-cookie-policy.php">Privacy &amp; cookie policy</a>
+			<a class="footer-link" href="/privacy-and-cookie-policy">Privacy &amp; cookie policy</a>
 		</p>
 		<p class="footer-copyright" >
 			&copy; <?php echo date("Y"); ?> <span itemprop="name">phoenix & phoenix</span>
