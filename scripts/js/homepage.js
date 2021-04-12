980ca96e1a96ff675c14b58f3c4e6d190778204b
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-04-12 11:04:10 +0100

    Removing some unused JS

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -9,13 +9,3 @@
 // *==============================================================================
 // ** Imported  **
 // *==============================================================================
-
-// const docWidth = document.documentElement.offsetWidth;
-
-// [].forEach.call(document.querySelectorAll('*'), function (el) {
-//   if (el.offsetWidth > docWidth) {
-//     console.log(el);
-//   }
-// });
-
-// console.log('Hi');
