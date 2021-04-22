d442a7afaece07ddb193c3aacb3143da8e73bd65
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-04-22 15:59:32 +0100

    Working wipe and swap

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -20,26 +20,42 @@ console.log('This is the homepage!');
 // *=========================================
 
 // ********** Initial Wipe **********
+
+/*
+Image wipes
+Images swap
+
+
+*/
+
 // * Element Variables
 const imageWipeElement = document.querySelectorAll('.image-wipe-animation');
 const imageSwapImages = gsap.utils.toArray('.image-wipe-animation img');
+console.log(imageSwapImages);
 
 gsap.set(imageWipeElement, { clipPath: 'inset(0 100% 0 0)' });
+
+const imageWipeSwap = gsap.timeline({
+  scrollTrigger: {
+    trigger: imageWipeElement,
+    start: 'top center',
+    markers: true,
+  },
+  defaults: { ease: 'none', duration: 0, delay: 0 },
+});
+
+imageWipeSwap
+  .to(imageWipeElement, { duration: 0.75, ease: 'circ.inOut', clipPath: 'inset(0% 0% 0% 0%)' })
+  .to(imageSwapImages[0], { autoAlpha: 0 }, '+=1')
+  .to(imageSwapImages[1], { autoAlpha: 1 })
+  .to(imageSwapImages[1], { autoAlpha: 0 }, '+=1')
+  .to(imageSwapImages[2], { autoAlpha: 1 });
 
 // ScrollTrigger.batch(imageWipeElement, {
 //   markers: true,
 //   start: 'top center',
 //   onEnter: (batch) => gsap.to(batch, { duration: 0.5, ease: 'circ.inOut', clipPath: 'inset(0% 0% 0% 0%)' }),
 // });
-
-imageWipeElement.forEach(function (elem) {
-  ScrollTrigger.create({
-    trigger: elem,
-    markers: true,
-    start: 'top center',
-    onEnter: () => gsap.to(elem, { duration: 0.5, ease: 'circ.inOut', clipPath: 'inset(0% 0% 0% 0%)' }),
-  });
-});
 
 // *==============================================================================
 // ** Page JS  **
