2e4d5f2123dcff1d71a86a7da3cae64382bb48cc
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-04-22 13:56:37 +0100

    GSAP batch

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -18,23 +18,42 @@ console.log('This is the homepage!');
 // *=========================================
 
 // ********** Initial Wipe **********
-
 const headerImageWrapper = document.querySelector('.homepage-header-image-wrapper');
+gsap.set(headerImageWrapper, { clipPath: 'inset(0 100% 0 0)' });
 
-gsap.fromTo(
-  headerImageWrapper,
-  { clipPath: 'inset(0 100% 0 0)' },
-  {
-    duration: 0.5,
-    ease: 'circ.inOut',
-    clipPath: 'inset(0% 0% 0% 0%)',
-    scrollTrigger: {
-      trigger: headerImageWrapper,
-      start: 'top center',
-      markers: true,
-    },
-  }
-);
+function imageWipeFunction() {
+  const imageWipeTl = gsap.timeline();
+  imageWipeTl.fromTo(headerImageWrapper, { clipPath: 'inset(0 100% 0 0)' }, { clipPath: 'inset(0% 0% 0% 0%)' });
+
+  return imageWipeTl;
+}
+
+ScrollTrigger.batch(headerImageWrapper, {
+  markers: true,
+  start: 'top center',
+  onEnter: (batch) =>
+    gsap.fromTo(
+      batch,
+      { clipPath: 'inset(0 100% 0 0)' },
+      { duration: 0.5, ease: 'circ.inOut', clipPath: 'inset(0% 0% 0% 0%)' }
+    ),
+  // you can also define things like start, end, etc.
+});
+
+// gsap.fromTo(
+//   headerImageWrapper,
+//   { clipPath: 'inset(0 100% 0 0)' },
+//   {
+//     duration: 0.5,
+//     ease: 'circ.inOut',
+//     clipPath: 'inset(0% 0% 0% 0%)',
+//     scrollTrigger: {
+//       trigger: headerImageWrapper,
+//       start: 'top center',
+//       markers: true,
+//     },
+//   }
+// );
 
 // *==============================================================================
 // ** Page JS  **
