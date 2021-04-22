4e7f7d88d15d5ddac2f21a0eec0622e608b47f29
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-04-22 13:16:19 +0100

    Initial image wipe animation

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -1,13 +1,41 @@
 // *==============================================================================
 // ** Imports  **
 // *==============================================================================
-// TODO: Temp 👇🏾 Remove
+// TODO: Temp for speedy animation building 👇🏾 Remove
 
 import { gsap } from 'gsap';
 import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
 import { animatedMainLogo } from './animations';
+
+// !==============================================================================
+// ! Temp Things For Building **
+// !==============================================================================
+console.log('This is the homepage!');
+// *=========================================
+// ** Header Image  **
+// *=========================================
+
+// ********** Initial Wipe **********
+
+const headerImageWrapper = document.querySelector('.homepage-header-image-wrapper');
+
+gsap.fromTo(
+  headerImageWrapper,
+  { clipPath: 'inset(0 100% 0 0)' },
+  {
+    duration: 0.5,
+    ease: 'circ.inOut',
+    clipPath: 'inset(0% 0% 0% 0%)',
+    scrollTrigger: {
+      trigger: headerImageWrapper,
+      start: 'top center',
+      markers: true,
+    },
+  }
+);
+
 // *==============================================================================
 // ** Page JS  **
 // *==============================================================================
@@ -20,7 +48,5 @@ import { animatedMainLogo } from './animations';
 // ** GSAP  **
 // *=========================================
 
+// * Animated Main Logo
 animatedMainLogo();
-
-const headerImageWrapper = document.querySelector('.homepage-header-image-wrapper');
-console.log(headerImageWrapper);
