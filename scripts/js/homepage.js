12880aa217a94c56bda95712486a55e76dcd7297
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-04-28 19:02:58 +0100

    Split text initial

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -27,11 +27,48 @@ const splitTextHeadings = document.querySelector('.split-text-heading-animation'
 
 const split = new SplitText(splitTextHeadings, { type: 'words' });
 
-gsap.fromTo(
-  split.words,
-  { opacity: 0, y: -20 },
-  { delay: 1, opacity: 1, duration: 0.75, y: 1, stagger: { amount: 0.5, from: 'random', ease: 'power2.inOut' } }
-);
+const splitWords = split.words;
+
+const evenSplit = splitWords.filter((div, index) => index % 2 === 0);
+const oddSplit = splitWords.filter((div, index) => index % 2 !== 0);
+
+function splitBeGone() {
+  split.revert();
+}
+
+const headingAnimationTL = gsap.timeline({
+  defaults: {
+    duration: 0.75,
+    ease: 'power2.inOut',
+    stagger: { each: 0.1, from: 'random' },
+  },
+  onComplete: splitBeGone,
+});
+
+// gsap.set(evenSplit, { y: -20 });
+// gsap.set(oddSplit, { y: 20 });
+
+headingAnimationTL
+  .addLabel('start', 0.5)
+  .fromTo(
+    evenSplit,
+    { opacity: 0, y: -20 },
+    {
+      opacity: 1,
+      y: 0,
+    },
+    'start'
+  )
+  .fromTo(
+    oddSplit,
+    { opacity: 0, y: +20 },
+    {
+      opacity: 1,
+      y: 0,
+    },
+    'start'
+  );
+
 // *==============================================================================
 // ** Page JS  **
 // *==============================================================================
