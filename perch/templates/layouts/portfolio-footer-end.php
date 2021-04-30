b247680be11094f87c98d3fd2e98981443dcea3d
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-04-30 19:27:47 +0100

    Almist there with adding numbers

--- perch/templates/layouts/portfolio-footer-end.php
+++ perch/templates/layouts/portfolio-footer-end.php
@@ -1,3 +1,4 @@
+<!-- TODO: Change to minified -->
 <script src="/scripts/minified/portfolio-min.js"></script>
 </script>
 </body>
--- scripts/js/portfolio.js
+++ scripts/js/portfolio.js
@@ -2,7 +2,7 @@
 // ** Imports  **
 // *==============================================================================
 
-import { animatedMainLogo } from './animations';
+import { animatedMainLogo, splitTextHeadingsFunction, bodySplitTextAnimation } from './animations';
 
 // *=========================================
 // ** GSAP  **
@@ -11,3 +11,23 @@ import { animatedMainLogo } from './animations';
 // ********** Animated Main Logo **********
 
 animatedMainLogo();
+splitTextHeadingsFunction();
+
+// *=========================================
+// ** On Page JS  **
+// *=========================================
+
+const menuItem = Array.from(document.querySelectorAll('.menu-item-number'));
+let count = 1;
+let preCount = 0;
+
+menuItem.forEach((item) => {
+  if (count > 9) {
+    preCount = '';
+  }
+  const itemCounter = document.createElement('span');
+  itemCounter.classList.add('item-number');
+  itemCounter.textContent = `${preCount}${count}`;
+  item.insertAdjacentElement('afterbegin', itemCounter);
+  count += 1;
+});
