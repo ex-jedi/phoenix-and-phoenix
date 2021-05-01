1776a8afb4d3b2e435ca1618aab091bcf46748a8
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-05-01 18:09:06 +0100

    Added number count to portfolio menu

--- scripts/js/portfolio.js
+++ scripts/js/portfolio.js
@@ -16,18 +16,3 @@ splitTextHeadingsFunction();
 // *=========================================
 // ** On Page JS  **
 // *=========================================
-
-const menuItem = Array.from(document.querySelectorAll('.menu-item-number'));
-let count = 1;
-let preCount = 0;
-
-menuItem.forEach((item) => {
-  if (count > 9) {
-    preCount = '';
-  }
-  const itemCounter = document.createElement('span');
-  itemCounter.classList.add('item-number');
-  itemCounter.textContent = `${preCount}${count}`;
-  item.insertAdjacentElement('afterbegin', itemCounter);
-  count += 1;
-});
