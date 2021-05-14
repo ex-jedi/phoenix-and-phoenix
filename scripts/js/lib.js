300641e38c214f621f9c87830a4acc8d939f0a10
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-05-14 14:05:44 +0100

    Tidy up

--- scripts/js/lib.js
+++ scripts/js/lib.js
@@ -1,56 +1,16 @@
+// *==============================================================================
+// ** Utils  **
+// *==============================================================================
+
 function consoleGreeting() {
   console.log(
     "Well hello 👋🏾, fancy seeing you here. As you're interested in this sort of thing why not have a look at my GitHub profile - 💻👍🏾 https://github.com/ex-jedi 👍🏾💻"
   );
 }
-// *=========================================
-// ** Main Nav  **
-// *=========================================
-// const mainNavTriggerWrapper = document.querySelector('.main-nav-trigger-wrapper');
-// const mainNavTrigger = document.querySelector('.main-nav-trigger');
-// const mainNav = document.querySelector('.main-nav');
-// const navLink = document.querySelectorAll('.main-nav-link');
-
-// // Restore pointerevents
-// function pointerEventsRestore() {
-//   mainNavTrigger.style.pointerEvents = 'auto';
-//   if (mainNav.dataset.state === 'open') {
-//     mainNavTrigger.textContent = 'CLOSE MENU';
-//     mainNavTrigger.style.padding = '0';
-//   } else {
-//     mainNavTrigger.textContent = 'MENU';
-//     mainNavTrigger.style.padding = '0 5rem';
-//     // Stripping out styles injected by GreenSock to show normal menu if screen is resized
-//     mainNav.removeAttribute('style');
-//     navLink.forEach((link) => link.removeAttribute('style'));
-//   }
-// }
-
-// function menuOpenerHandler() {
-//   if (mainNav.dataset.state === 'closed') {
-//     openMenuTl.restart();
-//     mainNavTrigger.style.pointerEvents = 'none';
-//     mainNav.dataset.state = 'open';
-//   } else {
-//     closeMenuTl.restart();
-//     mainNavTrigger.style.pointerEvents = 'none';
-//     mainNav.dataset.state = 'closed';
-//   }
-// }
 
 // *==============================================================================
-// ** GSAP Animations For Multiple Pages  **
+// ** Tabs  **
 // *==============================================================================
-
-// *==============================================================================
-// ** Homepage  **
-// *==============================================================================
-
-// *==============================================================================
-// ** Services  **
-// *==============================================================================
-
-// ********** Tabs **********
 
 function getTabsElements() {
   const tabs = document.querySelector('.tabs-wrapper');
