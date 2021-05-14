9bf9aef3251f175643308ad62ee99a251092087b
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-05-14 17:51:42 +0100

    Setting up nav animations

--- scripts/js/animations.js
+++ scripts/js/animations.js
@@ -61,31 +61,67 @@ function scrollTriggerRefresh(time = 2000) {
 // ** Nav  **
 // *=========================================
 
-// const openMenuTl = gsap.timeline({
-//   paused: true,
-//   defaults: { ease: 'power3.out', duration: 1, delay: 0 },
-// });
+// * Get nav elements
 
-// openMenuTl
-//   .to(mainNav, { y: '0%' })
-//   .addLabel('colorChange', '-=0.3')
-//   .to(navLink, { y: 0, opacity: 1, stagger: 0.2, duration: 0.5 }, 'colorChange')
-//   .to(mainNavTriggerWrapper, { backgroundColor: '#f4f1f0' }, 'colorChange')
-//   .to(mainNavTrigger, { color: '#6c9184', onComplete: pointerEventsRestore }, 'colorChange');
+function getNavElements() {
+  const mainNav = document.querySelector('.main-nav');
+  const mainNavLinks = gsap.utils.toArray(document.querySelectorAll('.main-nav-link'));
+  const mainNavTriggerWrapper = document.querySelector('.main-nav-trigger-wrapper');
+  const mainNavTrigger = document.querySelector('.main-nav-trigger');
 
-// // * Close menu
+  return { mainNav, mainNavLinks, mainNavTriggerWrapper, mainNavTrigger };
+}
 
-// const closeMenuTl = gsap.timeline({
-//   paused: true,
-//   defaults: { ease: 'power3.in', duration: 1, delay: 0 },
-// });
+// * Menu pointer events and text
+// Restore pointerevents
+function navTextPointerEvents() {
+  const { mainNav, mainNavLinks, mainNavTrigger } = getNavElements();
+  mainNavTrigger.style.pointerEvents = 'auto';
+  if (mainNav.dataset.state === 'open') {
+    mainNavTrigger.textContent = 'CLOSE MENU';
+    mainNavTrigger.style.padding = '0';
+  } else {
+    mainNavTrigger.textContent = 'MENU';
+    mainNavTrigger.style.padding = '0 5rem';
+    // Stripping out styles injected by GreenSock to show normal menu if screen is resized
+    mainNav.removeAttribute('style');
+    mainNavLinks.forEach((link) => link.removeAttribute('style'));
+  }
+}
 
-// closeMenuTl
-//   .to(navLink, { y: 40, opacity: 0, stagger: -0.2, duration: 0.5 })
-//   .addLabel('colorChange', '-=0.5')
-//   .to(mainNavTriggerWrapper, { backgroundColor: '#6c9184' }, 'colorChange')
-//   .to(mainNavTrigger, { color: '#f4f1f0' }, 'colorChange')
-//   .to(mainNav, { y: '120%', onComplete: pointerEventsRestore }, 'colorChange');
+// * Open Menu
+
+function menuOpenAnimation() {
+  const { mainNav, mainNavLinks, mainNavTriggerWrapper, mainNavTrigger } = getNavElements();
+  const openMenuTl = gsap.timeline({
+    paused: true,
+    defaults: { ease: 'power3.out', duration: 1, delay: 0 },
+  });
+
+  openMenuTl
+    .to(mainNav, { y: '0%' })
+    .addLabel('colorChange', '-=0.3')
+    .to(mainNavLinks, { y: 0, opacity: 1, stagger: 0.2, duration: 0.5 }, 'colorChange')
+    .to(mainNavTriggerWrapper, { backgroundColor: '#f4f1f0' }, 'colorChange')
+    .to(mainNavTrigger, { color: '#6c9184', onComplete: pointerEventsRestore }, 'colorChange');
+}
+
+// * Close menu
+
+function closeMenuAnimation() {
+  const { mainNav, mainNavLinks, mainNavTriggerWrapper, mainNavTrigger } = getNavElements();
+  const closeMenuTl = gsap.timeline({
+    paused: true,
+    defaults: { ease: 'power3.in', duration: 1, delay: 0 },
+  });
+
+  closeMenuTl
+    .to(mainNavLinks, { y: 40, opacity: 0, stagger: -0.2, duration: 0.5 })
+    .addLabel('colorChange', '-=0.5')
+    .to(mainNavTriggerWrapper, { backgroundColor: '#6c9184' }, 'colorChange')
+    .to(mainNavTrigger, { color: '#f4f1f0' }, 'colorChange')
+    .to(mainNav, { y: '120%', onComplete: pointerEventsRestore }, 'colorChange');
+}
 
 // *=========================================
 // ** Main Logo  **
