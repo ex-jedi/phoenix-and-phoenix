07247ef888987ecfcdef0738ee03ac9f2e873b0d
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-05-21 17:54:51 +0100

    Adding query string to illustrations js file

--- perch/templates/layouts/illustration-footer-end.php
+++ perch/templates/layouts/illustration-footer-end.php
@@ -1,3 +1,3 @@
-<script src="/scripts/minified/illustration-min.js"></script>
+<script src="/scripts/minified/illustration-min.js?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/scripts/minified/illustration-min.js');?>"></script>
 </body>
 </html>
