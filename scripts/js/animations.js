1810d7cc90b9842ec18c0577a9e61adec6f09753
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-05-24 14:32:52 +0100

    Some todo's and unused import in animations

--- scripts/js/animations.js
+++ scripts/js/animations.js
@@ -3,7 +3,7 @@
 // *=========================================
 
 // ********** GSAP **********
-import { gsap, timeline } from 'gsap';
+import { gsap } from 'gsap';
 import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
