f5416f57d4cf1a60bd1553e33914cde7351828ad
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-05-30 17:28:23 +0100

    Adding query string to legals js

--- perch/templates/layouts/legals-footer-end.php
+++ perch/templates/layouts/legals-footer-end.php
@@ -1,4 +1,4 @@
-<script src="/scripts/minified/legals-min.js"></script>
+<script src="/scripts/minified/legals-min.js?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/scripts/minified/illustration-min.js');?>"></script>
 </script>
 </body>
 </html>
